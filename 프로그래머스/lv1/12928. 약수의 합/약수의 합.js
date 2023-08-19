e7e01ed346b6f694a31d90a01e283c20db3bdaca
rladydqls99 <118970395+rladydqls99@users.noreply.github.com>
2023-08-19 15:24:33 +0900

    [level 1] Title: 약수의 합, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    for(let i=1; i<n+1; i++){
+        if(n % i === 0) {
+            answer += i
+        } else {
+            continue
+        }
+    }
+    return answer;
+}
