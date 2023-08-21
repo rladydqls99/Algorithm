146f115799cda3eb2c87dc7365cc3ce36f8861b8
rladydqls99 <118970395+rladydqls99@users.noreply.github.com>
2023-08-22 00:11:57 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,23 +1,27 @@
+// 이상한 문자열 만들기
 function solution(s) {
+    // s를 소문자로 먼저 변환
     s = s.toLowerCase();
     var answer = '';
 
-    // Split the string into words
+    // s를 공백으로 나눈 변수 words 생성
     let words = s.split(" ");
 
-    // Iterate over each word
+    // words 길이만큼 반복
     for (let i = 0; i < words.length; i++) {
+        // 개별 단어를 저장할 word 생성
         let word = "";
+        // words의 개별 단어의 길이만큼 반복
         for (let j = 0; j < words[i].length; j++) {
-            // If the index is even, convert the character to uppercase.
-            // Otherwise, keep it lowercase.
+            // 인덱스가 짝수라면 대문자로 변환해서 word에 저장
             if (j % 2 === 0) {
                 word += words[i][j].toUpperCase();
+            // 그렇지 않으면 그대로 word에 저장
             } else {
                 word += words[i][j];
             }
         }
-        // If it's not the last word, add a space after it
+        // words의 마지막 문자가 아닐 대 word + 공백, 아니면 그냥 word를 더함
         answer += (i < words.length - 1) ? (word + " ") : word;
     }
 
