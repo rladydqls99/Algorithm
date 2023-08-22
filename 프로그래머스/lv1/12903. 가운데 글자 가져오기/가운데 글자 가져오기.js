0fd0a6386965d2bbff13e6535c8d11219ba9e90a
rladydqls99 <118970395+rladydqls99@users.noreply.github.com>
2023-08-22 14:00:54 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,10 +1,12 @@
 function solution(s) {
-    var answer = '';
-    var idx = s.length / 2
-    if(Number.isInteger(idx) === true){
-        answer = s[idx-1] + s[idx]
-    }else{
-        answer = s[Math.floor(idx)]
-    }
-    return answer;
+  // 짝수일 경우 - [s의 길이 / 2 -1] , [s의 길이] return
+  // 홀수일 경우 - Math.floor[s의 길이 / 2 ] 번째 글자 return
+  let idx = 0
+  if(s.length % 2 === 0){
+    idx = s.length / 2
+    return s.substr(idx-1, 2)
+  }else{
+    idx = Math.floor(s.length/2)
+    return s.substr(idx,1)
+  }
 }
