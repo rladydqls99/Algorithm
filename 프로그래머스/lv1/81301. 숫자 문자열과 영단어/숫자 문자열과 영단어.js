8eee63464f2c3804beec72623ab1e5c389cf345a
rladydqls99 <118970395+rladydqls99@users.noreply.github.com>
2023-08-23 10:13:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    for(let i=0; i<numbers.length; i++){
+        let arr = s.split(numbers[i])
+        s = arr.join(i)
+    }
+    
+    return +s;
+}
