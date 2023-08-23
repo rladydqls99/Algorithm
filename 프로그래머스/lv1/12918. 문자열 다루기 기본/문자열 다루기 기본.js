e6b6dac3a7aaaaf37a9c34b45f1d014cea049f27
rladydqls99 <118970395+rladydqls99@users.noreply.github.com>
2023-08-23 13:46:55 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,17 +1,23 @@
+// 문자열 다루기
 function solution(s) {
     let arr = []
+
+    // 문자열 길이가 4이거나 6인 것 판별
     if(s.length === 4 || s.length === 6){
         s = s.split("")
     
         s.forEach((element) =>{
+            // s 배열의 요소를 number로 바꾼뒤
             element = Number(element)
+            // Not a Number가 아니면 arr 배열에 push
             if(!(isNaN(element))){
                 arr.push(element)
             }
         })
-        if(s.length === arr.length){
-            return true
-        }
+    // s의 길이와 arr의 길이가 같으면 true
+    if(s.length === arr.length){
+        return true
+    }
     }
     
     return false;
