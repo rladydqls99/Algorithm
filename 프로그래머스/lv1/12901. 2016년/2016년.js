9eb9cab8a51e5503bed652131c8096444b54b3d3
rladydqls99 <118970395+rladydqls99@users.noreply.github.com>
2023-08-28 14:53:23 +0900

    Update 2016년.js

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -3,10 +3,13 @@ function solution(a, b) {
   let week = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
   let days = 0;
   let day = 0;
+
+  // 자기 자신 이전 달의 일수를 모두 더하기
   for (let i = 0; i < a - 1; i++) {
     days += month[i];
   }
-  console.log(days);
+
+  // 요일 계산
   days += b - 1;
   day = days % 7;
   result = week[day];
