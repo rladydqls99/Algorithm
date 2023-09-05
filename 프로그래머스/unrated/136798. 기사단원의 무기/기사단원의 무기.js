a9455f75cc193566400f451fc9e75473637baac6
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2023-09-05 09:44:16 +0900

    [unrated] Title: 기사단원의 무기, Time: 3.26 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,8 +1,9 @@
-// 1. number를 돌면서 각각의 약수 개수 구하기
-// 2. 약수 개수가 limit보다 클 경우 공격력 = power
-// 3. 다 더해서 출력
+
+
+
 function solution(number, limit, power) {
     var answer = 0;
+    // 1. number를 돌면서 각각의 약수 개수 구하기
     for(let i=1; i<=number; i++){
         let divisor = 0;
         for(let j=1; j*j<=i; j++){
@@ -12,8 +13,10 @@ function solution(number, limit, power) {
                 divisor += 2
             }
         }
+        // 2. 약수 개수가 limit보다 클 경우 공격력 = power
         if(divisor > limit) {
             answer += power
+        // 3. 다 더해서 출력
         }else{
             answer += divisor
         }
