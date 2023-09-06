72ea5467b22be5d74d7245792a407f789e8a592d
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2023-09-06 09:50:44 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,10 +1,8 @@
 function solution(lottos, win_nums) {
     const rank = [6, 6, 5, 4, 3, 2, 1];
+    
+    let minCount = lottos.filter(item => win_nums.includes((item))).length
+    let zeroCount = lottos.filter(item => item ===0).length
 
-    let minCount = lottos.filter(v => win_nums.includes(v)).length;
-    let zeroCount = lottos.filter(v => !v).length;
-
-    const maxCount = minCount + zeroCount;
-
-    return [rank[maxCount], rank[minCount]];
+    return [rank[minCount+zeroCount], rank[minCount]];
 }
