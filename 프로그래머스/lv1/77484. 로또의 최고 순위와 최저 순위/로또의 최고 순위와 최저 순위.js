24d4a08e3a1e765e4a66f27e5dbf73f27e842024
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2023-09-06 09:44:19 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,54 +1,10 @@
 function solution(lottos, win_nums) {
-    var answer = [];
-    lottos = lottos.sort((a,b)=> b-a )
-    
-    let cnt = 0
-    let zeroCnt = 0
-    lottos.forEach((item) => {
-        if(win_nums.includes(item)) {
-           cnt++
-        }else if(item ===0){
-            zeroCnt++
-        }
-    })
-    switch(cnt + zeroCnt){
-        case 6:
-            answer.push(1)
-            break
-        case 5:
-        answer.push(2)
-            break
-        case 4:
-        answer.push(3)
-            break
-        case 3:
-        answer.push(4)
-            break
-        case 2:
-        answer.push(5)
-            break
-        default:
-            answer.push(6) 
-    }
-    
-    switch(cnt){
-        case 6:
-            answer.push(1)
-            break
-        case 5:
-        answer.push(2)
-            break
-        case 4:
-        answer.push(3)
-            break
-        case 3:
-        answer.push(4)
-            break
-        case 2:
-        answer.push(5)
-            break
-        default:
-            answer.push(6) 
-    }
-    return answer;
+    const rank = [6, 6, 5, 4, 3, 2, 1];
+
+    let minCount = lottos.filter(v => win_nums.includes(v)).length;
+    let zeroCount = lottos.filter(v => !v).length;
+
+    const maxCount = minCount + zeroCount;
+
+    return [rank[maxCount], rank[minCount]];
 }
