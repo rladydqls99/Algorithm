11641b612e02939c12a8e0875a0cf1beef02ceb1
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2023-09-09 18:41:00 +0900

    Update 체육복.js

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,22 +1,23 @@
 function solution(n, lost, reserve) {
+    // 반복문을 돌기 전에 lost = reserve인 값들을 제거해 줘야함
+    // 이유는 반복문은 순서대로 돌기 때문에 다른 조건문이 먼저 실행될 가능성이 있기 때문
+    newLost = lost.filter(item => !reserve.includes(item));
+    newReserve = reserve.filter(item => !lost.includes(item));
     
-    // Handle students who have lost their extra gym uniforms first
-    let realLost = lost.filter(student => !reserve.includes(student));
-    let realReserve = reserve.filter(student => !lost.includes(student));
+    newLost = newLost.sort((comp1, comp2) =>comp1 - comp2)
     
-    realLost.sort((a, b) => a - b);
-    
-    realLost.forEach((item) => {
-        if(realReserve.includes(item - 1)) {
-            let idx = realReserve.indexOf(item - 1);
-            realReserve.splice(idx, 1);
-        } else if(realReserve.includes(item + 1)) {
-            let idx = realReserve.indexOf(item + 1);
-            realReserve.splice(idx, 1);
-        } else {
-            n -= 1;
+    newLost.forEach((item) => {
+        if(newReserve.includes(item-1)){
+            let idx = newReserve.indexOf(item-1)
+            newReserve.splice(idx,1)    
         }
-    });
-
+        else if(newReserve.includes(item+1)){
+            let idx = newReserve.indexOf(item+1)
+            newReserve.splice(idx,1)
+        }else{
+            n -= 1
+        }
+    })
+    
     return n;
 }
