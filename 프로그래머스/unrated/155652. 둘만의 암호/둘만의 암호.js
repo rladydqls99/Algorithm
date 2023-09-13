cfd86a85270ab73ca0aeb3d60be8c7e46624a430
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2023-09-13 10:26:28 +0900

    [unrated] Title: 둘만의 암호, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,27 @@
+function solution(s, skip, index) {
+    let result = ''
+    let texts ='abcdefghijklmnopqrstuvwxyz'
+    
+    for(i=0; i<skip.length; i++){
+        texts = texts.includes(skip[i]) ? texts.replace(skip[i],'') : texts
+    }
+    for(j=0; j<s.length; j++){
+        let idx = texts.indexOf(s[j]) + index
+        if(idx >= texts.length){
+            idx -= texts.length
+        }
+        if(idx >= texts.length){
+            idx -= texts.length
+        }
+        if(idx >= texts.length){
+            idx -= texts.length
+        }
+        if(idx >= texts.length){
+            idx -= texts.length
+        }
+        result += texts[idx]
+    }
+    
+    
+   return result;
+}
