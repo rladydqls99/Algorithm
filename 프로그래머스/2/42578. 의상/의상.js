cf7a43758df485ee56b7c257c0b605a96e831a9e
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2023-12-26 23:38:07 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,16 @@
+function solution(clothes) {
+    var answer = 1;
+    let clothesMap = new Map()
+    let count = 1
+    
+    clothes.forEach(clothe =>{
+        clothesMap.set(clothe[1], (clothesMap.get(clothe[1]) || 0) + 1)
+    })
+    
+    for(let clothe of clothesMap.values()) {
+        answer *= clothe +1
+    }
+     
+   
+    return answer-1 ;
+}
