1e9e421d2ad151f6f178671f890e7798224bc9ec
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2023-12-29 15:02:38 +0900

    [level 2] Title: 올바른 괄호, Time: 4.63 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,24 +1,10 @@
 function solution(s){
-    var answer = true;
-    let stack = []
-    
-    for(let i=0; i<s.length; i++){
-        if(s[i]=== '(' && s[i+1] === ')'){
-            i++
-        }else {
-            stack.push(s[i])
-            if(stack[stack.length-2] === '(' && stack[stack.length-1] === ')'){
-                stack.pop()
-                stack.pop()
-            }
+    let cum = 0
+    for (let paren of s) {
+        cum += paren === '('? 1: -1
+        if(cum < 0) {
+            return false
         }
     }
-    if(stack[0] === '(' && stack[1] === ')'){
-       stack = []
-    }
-    if(stack.length){
-        return false
-    }
-
-    return answer;
+    return cum === 0? true: false;
 }
