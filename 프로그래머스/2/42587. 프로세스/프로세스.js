b50e4cb690c3073958bc2d9355c288188413bcac
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2023-12-30 18:44:51 +0900

    [level 2] Title: 프로세스, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,17 +1,17 @@
 function solution(priorities, location) {
     var answer = 0;
-    const arr = priorities.map((priority, index) => [priority, index])
+    const queue = priorities.map((priority, index) => [priority, index])
     
-    while(arr.length !== 0){
+    while(queue.length !== 0){
         const maxValue = Math.max(...priorities)
-        const [priority, index] = arr.shift()
+        const [priority, index] = queue.shift()
         
         if(priority >= maxValue) {
             answer++
             priorities[index] = 1
             if(index === location) break
         }else{
-            arr.push([priority,index])
+            queue.push([priority,index])
         }
         
     }
