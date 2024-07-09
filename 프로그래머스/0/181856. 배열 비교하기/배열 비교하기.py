e61e2a1648b756fbeafae2ca216b5e97de150197
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-07-09 20:24:41 +0900

    [level 0] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -4,12 +4,9 @@ def solution(arr1, arr2):
     elif len(arr1) < len(arr2):
         return -1
     else:
-        sum1 = 0
-        sum2 = 0
-        for i in range(len(arr1)):
-            sum1 += arr1[i]
-            sum2 += arr2[i]
-        
+        sum1 = sum(arr1)
+        sum2 = sum(arr2)
+    
         if sum1 > sum2:
             return 1
         elif sum1 < sum2:
