b88c0775d68eac9a9c536a6e47e937b5e3d53de7
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-07-10 22:35:51 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.py
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.py
@@ -1,2 +1,10 @@
 def solution(myString):
-    return myString.lower()
+    answer = ""
+    for i in myString:
+        if ord(i) < 95:
+            i = ord(i) + 32
+            answer += str(chr(i))
+        else:
+            answer += i
+
+    return answer
