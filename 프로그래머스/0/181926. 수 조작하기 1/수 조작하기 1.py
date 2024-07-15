f2c7b05aa8e62a0c2381194f7582bb2a9ddb4931
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-07-15 17:10:05 +0900

    [level 0] Title: 수 조작하기 1, Time: 10.25 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,11 +1,7 @@
 def solution(n, control):
-    for i in range(len(control)):
-        if control[i] == 'w':
-            n += 1
-        elif control[i] == 's':
-            n -= 1
-        elif control[i] == 'd':
-            n += 10
-        else:
-            n -= 10
+    dict = { 'w': 1, 's': -1, 'd': 10, 'a': -10}
+    
+    for i in control:
+        n += dict[i]
+        
     return n
