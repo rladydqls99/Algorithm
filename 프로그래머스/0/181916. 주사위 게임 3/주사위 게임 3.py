ee8e23d83a9b24756f5d6de15f39e4bf06f7ebf4
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-07-22 21:48:06 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -0,0 +1,21 @@
+def solution(a, b, c, d):
+    numberArr = [a, b, c, d]
+    numberSet = set(numberArr)
+    
+    maxNumber = max(numberSet, key=numberArr.count)
+    minNumber = min(numberSet, key=numberArr.count)
+    
+    if len(numberSet) == 1:
+        return 1111 * maxNumber
+    if len(numberSet) == 2:
+        newList = list(numberSet)
+        if numberArr.count(a) == 3 or numberArr.count(b) == 3 :
+            return (10 * maxNumber + minNumber)**2
+        else:
+            return (newList[0] + newList[1]) * abs(newList[0] - newList[1])
+    if len(numberSet) == 3:
+        numberSet.remove(maxNumber)
+        newList = list(numberSet)
+        return newList[0] * newList[1]
+    if len(numberSet) == 4:
+        return min(numberSet)
