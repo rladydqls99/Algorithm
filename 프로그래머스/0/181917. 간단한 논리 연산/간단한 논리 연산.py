87913051f450beed2e86f350e0a97eb0b8d1b8f8
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-07-22 21:14:43 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -1,20 +1,2 @@
 def solution(x1, x2, x3, x4):
-    answer = True
-    first = True
-    second = True
-    
-    if x1 or x2:
-        first = True
-    else:
-        first = False
-    if x3 or x4:
-        second = True
-    else:
-        second = False
-    if first and second:
-        answer = True
-    else:
-        answer = False
-    
-        
-    return answer
+    return (x1 or x2) and (x3 or x4)
