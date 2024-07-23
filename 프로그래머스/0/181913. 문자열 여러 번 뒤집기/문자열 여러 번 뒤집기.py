01afd2f3cf04101db7fca4f0879c1cf2f54fc8c1
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-07-23 21:57:27 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.80 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,13 +1,7 @@
 def solution(my_string, queries):
     my_list = list(my_string)
     
-    for query in queries:
-        front_list = my_list[:query[0]]
-        middle_list = my_list[query[0]: query[1] + 1]
-        back_list = my_list[query[1] + 1:]
-        
-        middle_list.reverse()
-        
-        my_list = front_list + middle_list + back_list
+    for s, e in queries:
+        my_list[s:e + 1] = my_list[s:e + 1][::-1]
         
     return ''.join(my_list)
