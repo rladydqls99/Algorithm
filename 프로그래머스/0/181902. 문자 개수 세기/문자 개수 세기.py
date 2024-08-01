8406adedba98e7fd91b2a36320fa39fc915cd2de
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-08-02 00:15:58 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,10 +1,3 @@
+
 def solution(my_string):
-    answer = []
-    for i in range(65, 65+26):
-        cnt = my_string.count(chr(i))
-        answer.append(cnt)
-    for i in range(97, 97+26):
-        cnt = my_string.count(chr(i))
-        answer.append(cnt)
-        
-    return answer
+    return [my_string.count(alphabet) for alphabet in 'abcdefghijklmnopqrstuvwxyz'.upper()+'abcdefghijklmnopqrstuvwxyz']
