d448488051566a0099891ce4372ce1426ae3117c
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-08-03 23:48:23 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.14 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,7 +1,2 @@
-def solution(n, k):
-    answer = []
-    for i in range(n+1):
-        if i%k == 0 and i !=0:
-            answer.append(i)
-        
-    return answer
+def solution(n, k):    
+    return [i for i in range(k, n+1, k) ]
