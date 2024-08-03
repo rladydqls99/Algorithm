54b69def895b86556d9bea91a4c08b04ca336cf0
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-08-04 00:49:42 +0900

    [level 0] Title: 글자 지우기, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,9 +1,6 @@
+
 def solution(my_string, indices):
-    my_string_arr = list(my_string)
-    
-    for i in indices:
-        my_string_arr[i] = '*'
-        
-    result = ''.join(my_string_arr)   
-    
-    return result.replace('*', '')
+    answer = ''
+    for i in range(len(my_string)):
+        if i not in indices:answer+=my_string[i]
+    return answer
