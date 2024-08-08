e7ff277f3216db152cd30ce914883703dcc8af93
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-08-08 23:04:31 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,9 +1,2 @@
 def solution(num_list):
-    a = 0
-    b = 0
-    for i in range(0, len(num_list), 2):
-        a += num_list[i]
-    for i in range(1, len(num_list), 2):
-        b += num_list[i]
-        
-    return a if a>=b else b
+    return max(sum(num_list[::2]), sum(num_list[1::2]))
