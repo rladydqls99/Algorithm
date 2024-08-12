ecb3caa76694caf24b88844c2cbb73fd61000386
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-08-12 21:42:23 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -1,6 +1,3 @@
+
 def solution(myString, pat):
-    reverse = myString[::-1]
-    reverse_pat = pat[::-1]
-    
-    index = reverse.find(reverse_pat)
-    return myString[:-index] if index != 0 else myString
+    return myString[0:myString.rfind(pat)] + pat
