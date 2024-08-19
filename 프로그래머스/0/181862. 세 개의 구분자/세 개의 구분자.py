7e51f4437fb38f60be2bffaa09518bbaab5da853
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-08-19 22:55:31 +0900

    [level 0] Title: 세 개의 구분자, Time: 9.54 ms, Memory: 23.4 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,4 +1,3 @@
 def solution(myStr):
-    answer = myStr.replace('a', '*').replace('b', '*').replace('c', '*')
-    
-    return " ".join(answer.split('*')).split() or ['EMPTY']
+    answer = myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ')
+    return answer.split() or ['EMPTY']
