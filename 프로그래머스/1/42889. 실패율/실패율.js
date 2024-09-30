b00640507a82ddd80f257590e670cf5a1ef0a2eb
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-09-30 17:23:39 +0900

    [level 1] Title: 실패율, Time: 31.80 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,27 @@
+function solution(N, stages) {
+    var answer = [];
+    const obj = {}
+    
+    for(let i=0; i<stages.length; i++){
+        if (stages[i] in obj) {
+            obj[stages[i]] += 1
+        } else {
+            obj[stages[i]] = 1
+        }
+    }
+    let totalCnt = stages.length
+    for (let i=1; i<=N; i++){
+        let successCnt = 0
+        if (obj[`${i}`]) {
+            successCnt = obj[`${i}`]
+        }
+        answer.push([i, successCnt/totalCnt])
+        totalCnt -= successCnt
+    }
+    const newAnswer = []
+    answer = answer.sort((a,b) => b[1] - a[1])
+    answer.forEach(key => {
+        newAnswer.push(key[0])
+    })
+    return newAnswer;
+}
