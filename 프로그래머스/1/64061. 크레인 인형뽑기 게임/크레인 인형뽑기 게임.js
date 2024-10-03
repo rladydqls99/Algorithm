0bbf2c39c719f31e23f0426a6c14b80d5c08e67e
Yongbin Kim <118970395+rladydqls99@users.noreply.github.com>
2024-10-03 21:36:27 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.23 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -5,13 +5,13 @@ function solution(board, moves) {
     moves.forEach(row => {
         for(let col=0; col<board.length; col++) {
             if (board[col][row-1] !== 0) {
-                stack.push(board[col][row-1])
-                board[col][row-1] = 0
-                if (stack[stack.length-1] === stack[stack.length-2]) {
+                if (stack[stack.length-1] === board[col][row-1]) {
                     answer += 2
                     stack.pop()
-                    stack.pop()
+                } else {
+                    stack.push(board[col][row-1])
                 }
+                board[col][row-1] = 0
                 break
             }
         }
